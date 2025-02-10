52d2e278c9739cc6c783f782b3546aa2c34ff785
tunashred <enachealexandru.traian@gmail.com>
2025-02-11 00:18:07 +0200

    Add (de)serialization static methods

--- live-chat-private-dtos/src/main/java/com/github/tunashred/privatedtos/ProcessedMessage.java
+++ live-chat-private-dtos/src/main/java/com/github/tunashred/privatedtos/ProcessedMessage.java
@@ -1,5 +1,7 @@
 package com.github.tunashred.privatedtos;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tunashred.dtos.MessageInfo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,4 +17,14 @@ public class ProcessedMessage {
     MessageInfo messageInfo;
     String processedMessage;
     boolean isCensored;
+
+    static public String serialize(ProcessedMessage message) throws JsonProcessingException {
+        ObjectMapper writer = new ObjectMapper();
+        return writer.writeValueAsString(message);
+    }
+
+    static public ProcessedMessage deserialize(String messageSerialized) throws JsonProcessingException {
+        ObjectMapper reader = new ObjectMapper();
+        return reader.readValue(messageSerialized, ProcessedMessage.class);
+    }
 }
