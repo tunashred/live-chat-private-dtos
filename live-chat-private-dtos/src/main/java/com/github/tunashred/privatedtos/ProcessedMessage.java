8f30cdf844bf3a373646f965b038eaf99baaa933
tunashred <enachealexandru.traian@gmail.com>
2025-04-06 23:23:29 +0300

    Add ProcessedMessage DTO

---
+++ live-chat-private-dtos/src/main/java/com/github/tunashred/privatedtos/ProcessedMessage.java
@@ -0,0 +1,29 @@
+package com.github.tunashred.privatedtos;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tunashred.dtos.UserMessage;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProcessedMessage {
+    String channelName;
+    UserMessage userMessage;
+    String originalMessage;
+    Boolean isModerated;
+
+    static public String serialize(ProcessedMessage processedMessage) throws JsonProcessingException {
+        ObjectMapper writer = new ObjectMapper();
+        return writer.writeValueAsString(processedMessage);
+    }
+
+    static public ProcessedMessage deserialize(String processedMessageSerialized) throws JsonProcessingException {
+        ObjectMapper reader = new ObjectMapper();
+        return reader.readValue(processedMessageSerialized, ProcessedMessage.class);
+    }
+}
