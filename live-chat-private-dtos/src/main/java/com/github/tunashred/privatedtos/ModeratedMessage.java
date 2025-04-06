3fb08ba4602102010874c03ee12e765da37d4ca5
tunashred <enachealexandru.traian@gmail.com>
2025-04-06 19:29:45 +0300

    Update to version 2

--- live-chat-private-dtos/src/main/java/com/github/tunashred/privatedtos/ModeratedMessage.java
+++ live-chat-private-dtos/src/main/java/com/github/tunashred/privatedtos/ModeratedMessage.java
@@ -2,29 +2,27 @@ package com.github.tunashred.privatedtos;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tunashred.dtos.MessageInfo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ProcessedMessage {
-    MessageInfo messageInfo;
-    String processedMessage;
-    boolean isCensored;
+public class ModeratedMessage {
+    String originalMessage;
+    String moderatedMessage;
 
-    static public String serialize(ProcessedMessage message) throws JsonProcessingException {
+    static public String serialize(ModeratedMessage message) throws JsonProcessingException {
         ObjectMapper writer = new ObjectMapper();
         return writer.writeValueAsString(message);
     }
 
-    static public ProcessedMessage deserialize(String messageSerialized) throws JsonProcessingException {
+    static public ModeratedMessage deserialize(String messageSerialized) throws JsonProcessingException {
         ObjectMapper reader = new ObjectMapper();
-        return reader.readValue(messageSerialized, ProcessedMessage.class);
+        return reader.readValue(messageSerialized, ModeratedMessage.class);
     }
 }
